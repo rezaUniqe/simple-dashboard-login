96071de91ba32c608ff7a4ac618a2a2f6f037c80
rgo <rzgggg12@gmail.com>
2025-06-13 10:28:16 +0330

    added current user hook

--- src/hooks/use-current-user.ts
+++ src/hooks/use-current-user.ts
@@ -4,20 +4,24 @@ import {User, userSchema} from "@/schema/user";
 
 export function useCurrentUser() {
     const router = useRouter()
-    const [user, setUser] = useState<User| null>(null)
-
+    const [user, setUser] = useState<User | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
     useEffect(() => {
         const userData = localStorage.getItem("user")
+        setLoading(false)
         if (userData) {
             try {
-                setUser(userSchema.parse(userData))
-            }catch (e) {
+                const user = userSchema.parse(JSON.parse(userData))
+                setUser(user)
+            } catch (e) {
+                setLoading(false)
+
                 setUser(null)
             }
         }
     }, [router])
 
-   return user
+    return {user, isLoadingUser: loading}
 
 
 }
